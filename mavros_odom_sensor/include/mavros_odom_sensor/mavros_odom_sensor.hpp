2c9f58efb9f5a9449c688f4454df6122a05f4d40
JBVAkshaya <agrawaak@oregonstate.edu>
2025-02-18 19:12:30 -0800

    mavros odom sensor, creates state interfaces from topic

--- mavros_odom_sensor/include/mavros_odom_sensor/mavros_odom_sensor.hpp
+++ mavros_odom_sensor/include/mavros_odom_sensor/mavros_odom_sensor.hpp
@@ -1,103 +1,57 @@
-// Copyright 2024, Evan Palmer
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-#pragma once
+// mavros_odom_sensor.hpp
+#ifndef SAMPLE_SENSOR_HPP_
+#define SAMPLE_SENSOR_HPP_
 
 #include <memory>
 #include <vector>
 
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
-#include "hardware_interface/system_interface.hpp"
-#include "hardware_interface/types/hardware_interface_return_values.hpp"
-#include "mavros_msgs/msg/override_rc_in.hpp"
 #include "rcl_interfaces/msg/parameter.hpp"
 #include "rcl_interfaces/srv/set_parameters.hpp"
-#include "rclcpp/macros.hpp"
 #include "rclcpp/node.hpp"
 #include "rclcpp/time.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
-#include "realtime_tools/realtime_buffer.h"
-#include "realtime_tools/realtime_publisher.h"
-#include "mavros_odom_sensor/visibility_control.h"
 
 #include "nav_msgs/msg/odometry.hpp"
 
+#include "hardware_interface/sensor_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
+
 namespace mavros_odom_sensor
 {
-
-class MavrosOdomSensor : public hardware_interface::SystemInterface
+class MavrosOdomSensor : public hardware_interface::SensorInterface
 {
-  RCLCPP_SHARED_PTR_DEFINITIONS(MavrosOdomSensor)
+public:
+  RCLCPP_SHARED_PTR_DEFINITIONS(MavrosOdomSensor)  // Remove semicolon, it's included in the macro
   hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
 
-  MAVROS_ODOM_SENSOR_PUBLIC
-  hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+  hardware_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State& previous_state) override;
 
-  MAVROS_ODOM_SENSOR_PUBLIC
   hardware_interface::CallbackReturn on_cleanup(const rclcpp_lifecycle::State & previous_state) override;
 
-  MAVROS_ODOM_SENSOR_PUBLIC
-  hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
-
-  MAVROS_ODOM_SENSOR_PUBLIC
-  hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
-
-  MAVROS_ODOM_SENSOR_PUBLIC
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
-  MAVROS_ODOM_SENSOR_PUBLIC
-  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+  hardware_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State& previous_state) override;
 
-  MAVROS_ODOM_SENSOR_PUBLIC
-  hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+  hardware_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State& previous_state) override;
 
-  MAVROS_ODOM_SENSOR_PUBLIC
-  hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+  hardware_interface::return_type read(
+    const rclcpp::Time& time, const rclcpp::Duration& period) override;
 
-protected:
-  struct ThrusterConfig
-  {
-    rcl_interfaces::msg::Parameter param;
-    int channel;
-    int neutral_pwm = 1500;
-  };
-
-  void stop_thrusters();
-  std::vector<std::string> split(const std::string &string_val, char delimiter);
-  std::string concatenate_strings(const std::vector<std::string> &vec);
-
-  // We need a node to interact with MAVROS
-  std::shared_ptr<rclcpp::Node> node_;
   std::shared_ptr<rclcpp::Node> node_odom_;
 
-  std::shared_ptr<rclcpp::Publisher<mavros_msgs::msg::OverrideRCIn>> override_rc_pub_;
-  std::unique_ptr<realtime_tools::RealtimePublisher<mavros_msgs::msg::OverrideRCIn>> rt_override_rc_pub_;
-
-  std::shared_ptr<rclcpp::Client<rcl_interfaces::srv::SetParameters>> set_params_client_;
-  std::vector<ThrusterConfig> thruster_configs_;
-
-  int max_retries_;
   std::string namespace_;
-  std::vector<double> hw_commands_pwm_;
+  std::string mavros_odometry_topic_;
 
   // Params for creating state interfaces from odometry
   struct StateInterfaceConfig
@@ -110,7 +64,8 @@ protected:
   geometry_msgs::msg::Twist euler_from_quaternion(const nav_msgs::msg::Odometry odom_with_quaternion);
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr topic_based_odometry_subscriber_;
   StateInterfaceConfig state_interface_config_;
-  std::string is_odom_state_interface_configured_;
 };
 
-}  // namespace mavros_odom_sensor
+} // namespace mavros_odom_sensor
+
+#endif // SAMPLE_SENSOR_HPP_
